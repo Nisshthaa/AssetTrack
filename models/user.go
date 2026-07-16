8623a941ffbae370268369dcfdede29f3c46155a
nisshthaa <sharmanishtha791@gmail.com>
2026-07-16 15:18:05 +0530

    made some changes

--- models/user.go
+++ models/user.go
@@ -5,7 +5,7 @@ import "time"
 type RegisterUser struct {
 	Name        string `json:"name" db:"name" validate:"required,min=3,max=50"`
 	Email       string `json:"email" db:"email" validate:"required,email"`
-	Role        string `json:"role" db:"role" validate:"required,oneof=admin employee project_manager asset_manager employee_manager"`
+	Role        string `json:"role" db:"role" validate:"required,oneof=admin employee project_manager"`
 	RoleType    string `json:"roleType" db:"type" validate:"required,oneof=full-time intern freelancer" `
 	PhoneNumber string `json:"phoneNumber" db:"phone_no" validate:"required,len=10"`
 	Password    string `json:"password" db:"password" validate:"required,min=8,max=20"`
